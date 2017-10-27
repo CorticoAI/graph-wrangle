7f9502ffbc3364174d1f1c457f31a641ee15d561
Peter Beshai <peter.beshai@gmail.com>
2017-10-27 15:43:31 -0400

    Get working example of pipeline command

--- bin/graph-wrangle.js
+++ bin/graph-wrangle.js
@@ -11,4 +11,7 @@ if (process.env['DEBUG'] == null) {
 
 const cli = require('../lib/cli');
 
+// show stack trace on unhandled promise rejection (makes debugging easier)
+// see: https://github.com/nodejs/node/issues/9523#issuecomment-259303079
+process.on('unhandledRejection', r => console.error(r));
 cli(process.argv);
